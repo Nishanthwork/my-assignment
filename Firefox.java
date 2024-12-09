8e92dabeeaaecba4bd950f37ae5bb2382bdaad00
Nishanthwork <nishanthdanielz@gmail.com>
2024-12-09 11:39:11 +0530

    Add files via upload
    Firefox Value

--- Firefox.java
+++ Firefox.java
@@ -2,16 +2,16 @@ package Week1.day2;
 
 public class Firefox {
 public static void main(String[] args) {
-	float browserversion = 100.2f;
-	String browsername ="firefox";
+	float browserVersion = 100.2f;
+	String browserName ="firefox";
 	Boolean isVisible =true;
 	int releaseYear =1998;
 	char BrowserLogo ='f';
-	System.out.println("browserversion");
-	System.out.println("browsername");
-	System.out.println("isvisible");
-	System.out.println("releaseYear");
-	System.out.println("BrowserLogo");
+	System.out.println(browserVersion);
+	System.out.println(browserName);
+	System.out.println(isVisible);
+	System.out.println(releaseYear);
+	System.out.println(BrowserLogo);
 	
 	
 }
