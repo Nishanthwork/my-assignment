069322fd0410cbf4a21a148cc1baa9c79d63add9
Nishanthwork <nishanthdanielz@gmail.com>
2024-12-09 10:48:04 +0530

    Add files via upload
    chrome Assignment added

---
+++ Chrome.java
@@ -0,0 +1,23 @@
+package Week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float browserversion =91.0f;
+		String browsername="Google";
+		Boolean Betavalue = false;
+		int releaseyear=2008;
+		char shortcutkey ='C';
+		System.out.println("browserversion");
+		System.out.println("browsername");
+		System.out.println("betavalue");
+		System.out.println("releaseyear");
+		System.out.println("shortcutkey");
+		
+		
+		
+
+	}
+
+}
