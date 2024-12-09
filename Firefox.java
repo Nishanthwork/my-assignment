17b2e2e242de7d6a3d907938e9cd9f585af18073
Nishanthwork <nishanthdanielz@gmail.com>
2024-12-09 10:48:53 +0530

    Add files via upload
    Firefox assignment added

---
+++ Firefox.java
@@ -0,0 +1,18 @@
+package Week1.day2;
+
+public class Firefox {
+public static void main(String[] args) {
+	float browserversion = 100.2f;
+	String browsername ="firefox";
+	Boolean isVisible =true;
+	int releaseYear =1998;
+	char BrowserLogo ='f';
+	System.out.println("browserversion");
+	System.out.println("browsername");
+	System.out.println("isvisible");
+	System.out.println("releaseYear");
+	System.out.println("BrowserLogo");
+	
+	
+}
+}
